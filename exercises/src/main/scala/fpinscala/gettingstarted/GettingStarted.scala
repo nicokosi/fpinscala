fe4d696d039792b46c02c5210f38797ed3ea61ee
Nicolas Kosinski <nico_kosi-dev@yahoo.com>
2013-08-02 13:51:52 +0200

    "Getting started" (in progress)

--- exercises/src/main/scala/fpinscala/gettingstarted/GettingStarted.scala
+++ exercises/src/main/scala/fpinscala/gettingstarted/GettingStarted.scala
@@ -129,28 +129,54 @@ object PolymorphicFunctions {
 
   // Exercise 2: Implement a polymorphic function to check whether 
   // an `Array[A]` is sorted
-  def isSorted[A](as: Array[A], gt: (A,A) => Boolean): Boolean = ??? 
+  def isSorted[A](as: Array[A], gt: (A,A) => Boolean): Boolean = {
+    @annotation.tailrec
+    def go(i: Int): Boolean = {
+      if (i + 1 >= as.length) true
+      else if (gt(as(i), as(i + 1))) false     
+      else go(i + 1)
+    } 
+    go(0)
+  }
+
+  val greaterThan = new Function2[Int, Int, Boolean] {
+    def apply(a: Int, b: Int) = a > b
+  }
+  isSorted(Array(1, 2, 3), greaterThan)
+  isSorted(Array(1, 3, 2), greaterThan)
   
   // Polymorphic functions are often so constrained by their type
   // that they only have one implementation! Here's an example:
 
   // Exercise 3: Implement `partial1`.
-  
+/*  
   def partial1[A,B,C](a: A, f: (A,B) => C): B => C = 
-    ??? 
+      f(a, _)
+
+  val cities = Map(
+    "FR" -> Map(94800 -> "Villejuif"),
+    "US" -> Map(1 -> "Boston"))
+  def city(countryCode: String, zipCode: Int): Option[String] =
+    cities(countryCode).get(zipCode)
+  def franceCity = partial1("FR", city) franceCity(94800)
+*/  
   
   // Exercise 4: Implement `curry`.
 
   // Note that `=>` associates to the right, so we could 
   // write the return type as `A => B => C`
+/*  
   def curry[A,B,C](f: (A, B) => C): A => (B => C) =
-    ??? 
+    ???
+*/    
 
   // NB: The `Function2` trait has a `curried` method already
 
   // Exercise 5: Implement `uncurry`
+/*
   def uncurry[A,B,C](f: A => B => C): (A, B) => C =
     ??? 
+*/
 
   /*
   NB: There is a method on the `Function` object in the standard library,
@@ -163,7 +189,8 @@ object PolymorphicFunctions {
   */
 
   // Exercise 6: Implement `compose`
-
+/*
   def compose[A,B,C](f: B => C, g: A => B): A => C =
     ??? 
+*/    
 }
