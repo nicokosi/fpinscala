53930fa4c5dfe4cb7a04765444743e0c457a97a8
Nicolas Kosinski <nico_kosi-dev@yahoo.com>
2013-08-16 09:03:02 +0200

    Started "Functional data structures" exercises 1, 2 and 3.

--- exercises/src/main/scala/fpinscala/datastructures/List.scala
+++ exercises/src/main/scala/fpinscala/datastructures/List.scala
@@ -51,11 +51,23 @@ object List { // `List` companion object
     foldRight(l, 1.0)(_ * _)
 
 
-  def tail[A](l: List[A]): List[A] = sys.error("todo")
+  def tail[A](l: List[A]): List[A] = l match {
+    case Nil => Nil
+    case Cons(_, t) => t
+  }
 
-  def drop[A](l: List[A], n: Int): List[A] = sys.error("todo")
+  def drop[A](l: List[A], n: Int): List[A] = l match {
+    case Nil => l
+    case Cons(_, t) => if (n >0) drop (t, n-1)
+                       else t
+  }
 
-  def dropWhile[A](l: List[A])(f: A => Boolean): List[A] = sys.error("todo")
+  def dropWhile[A](l: List[A])(f: A => Boolean): List[A] = l match {
+    case Nil =>   Nil
+    case Cons(h, t) => if (f(h)) dropWhile(t)(f)
+                      else l
+  }
+  
 
   def setHead[A](l: List[A])(h: A): List[A] = sys.error("todo")
 
