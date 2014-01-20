49b8ee59fecef5465d4bc22744cc704de9ef7aa9
nicokosi <nico_kosi-dev@yahoo.com>
2014-01-20 11:44:05 +0100

    state Did again ex 1 to 6

--- exercises/src/main/scala/fpinscala/state/State.scala
+++ exercises/src/main/scala/fpinscala/state/State.scala
@@ -6,12 +6,12 @@ trait RNG {
 }
 
 object RNG {
-  def simple(seed: Long): RNG = new RNG {
-    def nextInt = {
-      val seed2 = (seed*0x5DEECE66DL + 0xBL) & // `&` is bitwise AND
-                  ((1L << 48) - 1) // `<<` is left binary shift
-      ((seed2 >>> 16).asInstanceOf[Int], // `>>>` is right binary shift with zero fill
-       simple(seed2))
+  case class Simple(seed: Long) extends RNG {
+    def nextInt: (Int, RNG) = {
+      val newSeed = (seed * 0x5DEECE66DL + 0xBL) & 0xFFFFFFFFFFFFL // `&` is bitwise AND. We use the current seed to generate a new seed.
+      val nextRNG = Simple(newSeed) // The next state, which is an `RNG` instance created from the new seed.
+      val n = (newSeed >>> 16).toInt // `>>>` is right binary shift with zero fill. The value `n` is our new pseudo-random integer.
+      (n, nextRNG) // The return value is a tuple containing both a pseudo-random integer and the next `RNG` state.
     }
   }
 
@@ -20,7 +20,7 @@ object RNG {
   val int: Rand[Int] = _.nextInt
 
   def unit[A](a: A): Rand[A] =
-    rng => (a, rng) 
+    rng => (a, rng)
 
   def map[A,B](s: Rand[A])(f: A => B): Rand[B] =
     rng => {
@@ -28,64 +28,84 @@ object RNG {
       (f(a), rng2)
     }
 
-  import scala.math.abs  
-  def positiveInt(rng: RNG): (Int, RNG) = rng.nextInt match {
-    case (Int.MinValue, next) => positiveInt(next)
-    case (i, next) => (abs(i), next)
+  def positiveInt(rng: RNG): (Int, RNG) = {
+    // exercise 1:
+    val (num, gen) = rng.nextInt
+    if (num == Integer.MAX_VALUE) 
+      positiveInt(gen)
+    else 
+      (num.abs, gen)
   }
 
-  def double(rng: RNG): (Double, RNG) = {
-    val (i, next) = positiveInt(rng)
-    val d = i / (Int.MaxValue.toDouble + 1)
-    (d, next)
-  }    
+  def double(rng: RNG): (Double, RNG) = {    
+    /*
+    // exercise 2 (raw implementation):
+    val (n, rng2) = positiveInt(rng)
+    val d = n.toDouble / Integer.MAX_VALUE
+    (d, rng2)
+    */
+
+    // exercise 5 (more elegant implementation using 'map' combinator):
+    map(positiveInt)(n => n.toDouble / Integer.MAX_VALUE)(rng)
+  }
 
   def intDouble(rng: RNG): ((Int,Double), RNG) = {
-    val (i, rng2) = rng.nextInt
+    /*
+    // exercise 3 (raw implementation):
+    val (i, rng2) = positiveInt(rng)
     val (d, rng3) = double(rng2)
     ((i, d), rng3)
+    */
+
+    // exercise 6 (more elegant implementation using 'map2' combinator):
+    both(positiveInt, double)(rng)
   }
 
   def doubleInt(rng: RNG): ((Double,Int), RNG) = {
+    /*
+    // exercise 3 (raw implementation):
     val ((i, d), rng2) = intDouble(rng)
     ((d, i), rng2)
+    */
+
+    // exercise 6 (more elegant implementation using 'map2' combinator):
+    both(double, positiveInt)(rng)
   }
+
+  def both[A,B](ra: Rand[A], rb: Rand[B]): Rand[(A,B)] =
+    map2(ra, rb)((_, _))
+
 
   def double3(rng: RNG): ((Double,Double,Double), RNG) = {
-    val (d1, rng2) = double(rng)
-    val (d2, rng3) = double(rng2)
-    val (d3, rng4) = double(rng3)
-    ((d1, d2, d3), rng4)
+    // exercise 3
+    val (d1, rng1) = double(rng)
+    val (d2, rng2) = double(rng1)
+    val (d3, rng3) = double(rng2)
+    ((d1, d2, d3), rng3)
   }
 
-  def ints(count: Int)(rng: RNG): (List[Int], RNG) = {
-    def go(counter: Int, l: List[Int], rng: RNG) : (List[Int], RNG) = {
-      if (counter == 0) {
-        (l, rng)
-      } else {
-        val (i, rng2) = rng.nextInt
-        go(counter - 1, i :: l, rng2)
-      }      
+  def ints(count: Int)(rng: RNG): (List[Int], RNG) = { // exercise 4
+    @annotation.tailrec
+    def addInt(rng: RNG, count: Int, list: List[Int]): (List[Int], RNG) = {
+      if (count == 0) (list, rng)
+      else {
+        val (i, rng2) = positiveInt(rng)
+        addInt(rng2, count - 1, i :: list)
+      }
     }
-    go(count, List(), rng)
+    addInt(rng, count, List())
   }
 
-  def positiveMax(n: Int): Rand[Int] = sys.error("todo")
+  def map2[A,B,C](ra: Rand[A], rb: Rand[B])(f: (A, B) => C): Rand[C] = rng => {
+    // exercise 6
+    val (a, rng2) = ra(rng)
+    val (b, rng3) = rb(rng2)
+    (f(a,b), rng3)
+  }
 
-  def map2[A,B,C](ra: Rand[A], rb: Rand[B])(f: (A, B) => C): Rand[C] =
-    rng => {
-      val (a, rng2) = ra(rng)
-      val (b, rng3) = rb(rng2)
-      (f(a, b), rng3)
-    }
+  def sequence[A](fs: List[Rand[A]]): Rand[List[A]] = ???
 
-  def sequence[A](fs: List[Rand[A]]): Rand[List[A]] = sys.error("todo")
-
-  def flatMap[A,B](f: Rand[A])(g: A => Rand[B]): Rand[B] =
-    rng => {
-      val (a, rng2) = f(rng)
-      g(a)(rng2)
-    }
+  def flatMap[A,B](f: Rand[A])(g: A => Rand[B]): Rand[B] = ???
 }
 
 case class State[S,+A](run: S => (A, S)) {
@@ -105,54 +125,5 @@ case class Machine(locked: Boolean, candies: Int, coins: Int)
 
 object State {
   type Rand[A] = State[RNG, A]
-  def simulateMachine(inputs: List[Input]): State[Machine, Int] = sys.error("todo")
-}
-
-object StateMain {
-
-  import fpinscala.state.RNG._
-  def main(args: Array[String]): Unit = {
-    // ex1
-    val gen = simple(42)
-    println("positive int: " + positiveInt(gen))
-    println
-    // ex2
-    println("double [0..1[: " + double(gen))
-    println
-    // ex3
-    println("intDouble: " + intDouble(gen))
-    println("doubleInt: " + doubleInt(gen))
-    println("double3: " + double3(gen))
-    println
-    // ex4
-    println("10 random ints: " + ints(10)(gen))
-    println
-    // ex5
-    def doubleViaMap =
-      map(positiveInt)(i => i / (Int.MaxValue.toDouble + 1))
-    println("double [0..1[ via map: " + doubleViaMap(gen))
-    println
-    // ex6
-    def both[A,B](ra: Rand[A], rb: Rand[B]): Rand[(A,B)] =
-      map2(ra, rb)((_, _))
-    val randIntDouble: Rand[(Int, Double)] =
-      both(int, double)
-    println("randIntDouble: " + randIntDouble(gen))
-    println
-    // ex8
-    def positiveLessThan(n: Int): Rand[Int] =
-      flatMap(positiveInt) { i =>
-        val mod = i % n
-        if (i + (n-1) - mod > 0) unit(mod)
-        else positiveLessThan(n)
-      }
-    println("positiveLessThan 42: " + positiveLessThan(42)(gen))
-    // ex 9
-    def mapViaFlatMap[A,B](s: Rand[A])(f: A => B): Rand[B] =
-      flatMap(s) { i => unit(f(i)) }
-    def doubleViaMapViaFlatMap =
-      mapViaFlatMap(positiveInt)(i => i / (Int.MaxValue.toDouble + 1))
-    println("doubleViaMapViaFlatMap : " + doubleViaMapViaFlatMap(gen))
-  }
-
+  def simulateMachine(inputs: List[Input]): State[Machine, (Int, Int)] = ???
 }
